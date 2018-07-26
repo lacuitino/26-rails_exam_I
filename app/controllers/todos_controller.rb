3bd1b663de0f9553dbea857d4747d87dc56675ea
Lucas Cuitino <cuitino.lu@gmail.com>
2018-07-26 19:47:15 -0400

    filtered and created

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,5 +7,16 @@ class TodosController < ApplicationController
   def new
     @todo = Todo.new
   end
-  
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description,:completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
  #creacoin de ruta
   get 'todos', to: 'todos#index'
   get 'todos/new'
+
+  get 'todos', to: 'todos#create'
+  
 end
